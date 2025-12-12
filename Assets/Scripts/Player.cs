af8473a29ef1adb311d2749b48d37578234f9dc7
JugieNoob <jurgenarna@gmail.com>
2025-12-12 10:50:56 +0100

    Better player movement and player can shoot lasers

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    float moveSpeed = 0.025f;
+    [SerializeField] float moveSpeed = 3f;
 
     [SerializeField] GameObject playerLaser;
+    [SerializeField] float laserVelocity = 5f;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,23 +20,23 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.UpArrow) && Camera.main.WorldToScreenPoint(transform.position).y < (Camera.main.pixelHeight - 16))
         {
-            transform.position = new Vector2(transform.position.x, transform.position.y + moveSpeed);
+            transform.position = new Vector2(transform.position.x, transform.position.y + (moveSpeed * Time.deltaTime));
         }
         else if (Input.GetKey(KeyCode.DownArrow) && Camera.main.WorldToScreenPoint(transform.position).y > 16)
         {
-            transform.position = new Vector2(transform.position.x, transform.position.y - moveSpeed);
+            transform.position = new Vector2(transform.position.x, transform.position.y - (moveSpeed * Time.deltaTime));
         }
 
 
         if (Input.GetKey(KeyCode.LeftArrow) && Camera.main.WorldToScreenPoint(transform.position).x > 16)
         {
-            transform.position = new Vector2(transform.position.x - moveSpeed, transform.position.y);
+            transform.position = new Vector2(transform.position.x - (moveSpeed * Time.deltaTime), transform.position.y);
         }
         else if (Input.GetKey(KeyCode.RightArrow) && Camera.main.WorldToScreenPoint(transform.position).x < (Camera.main.pixelWidth - 16))
         {
-            transform.position = new Vector2(transform.position.x + moveSpeed, transform.position.y);
+            transform.position = new Vector2(transform.position.x + (moveSpeed * Time.deltaTime), transform.position.y);
         }
-
+        
         if (Input.GetKeyDown(KeyCode.Z))
         {
             ShootLaser();
@@ -45,5 +46,7 @@ public class Player : MonoBehaviour
     void ShootLaser()
     {
         GameObject laser = Instantiate(playerLaser, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y), Quaternion.identity);//gameObject.transform
+
+        laser.GetComponent<Rigidbody2D>().linearVelocityY = laserVelocity;
     }
 }
