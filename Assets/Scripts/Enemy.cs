02a26674c0ed520ea78dc1b1448156b58fdda62d
JugieNoob <jurgenarna@gmail.com>
2025-12-17 14:05:29 +0100

    Enemies can shoot lasers

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] GameObject enemyLaser;
+    [SerializeField] float laserVelocity = 5f;
+    [SerializeField] float timeBetweenShots = 1.5f;
+
+    void Start()
+    {
+        StartCoroutine(ShootLaser());
+    }
+
+    IEnumerator ShootLaser()
+    {
+        while (true)
+        {
+            GameObject laser = Instantiate(enemyLaser, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y), Quaternion.identity);
+            laser.GetComponent<Rigidbody2D>().linearVelocityY = -laserVelocity;
+            yield return new WaitForSeconds(timeBetweenShots);
+        }
+
+    }
+
+}
